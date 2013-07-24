8dd56d0bdbd2cb8de2a96c0524aeb6a179ddc289
Alexander Standke <xanderstrike@gmail.com>
2013-07-24 13:30:20 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chattr::Application.config.secret_token = '2324814e6ad80e40c0b13d61648b0e91b1bfd7badd8e6315f58565cfedf21660462ff6f25b8cc6f4e5cbf4947746228006e2b5c3a033f9a60504e7725dad58b4'
